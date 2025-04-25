180775c36f5af1586b27c72670ea902ec15990ac
Adam Booth <10toasterv2@gmail.com>
2025-04-25 14:36:25 -0600

    express set up

---
+++ server.js
@@ -0,0 +1,23 @@
+// Import express using ESM syntax
+import express from 'express';
+ 
+// Create an instance of an Express application
+const app = express();
+ 
+const name = process.env.NAME; // <-- NEW
+
+// Define a route handler for the root URL ('/')
+app.get('/', (req, res) => {
+    res.send(`Hello, ${name}! <a href="/about-me">About Me</a>`);
+});
+app.get('/about-me', (req, res) => {
+    res.send('<h1>About Me</h1><p>This is a simple Express Server</p><a href="/">Home</a>');
+});
+ 
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+ 
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
